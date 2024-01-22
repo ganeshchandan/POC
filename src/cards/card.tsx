9637fd58c426682a637f3f1c42474693157443db
ganeshchandan <chandan.ganesh92@gmail.com>
2024-01-22 13:44:45 +0530

    Animation changes

--- src/cards/card.tsx
+++ src/cards/card.tsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 
-const  debounce = (func :  () => void, timeout = 2000) =>{
+const  debounce = (func :  () => void, timeout = 1000) =>{
     let timer : NodeJS.Timeout ;
     return () => {
       clearTimeout(timer);
