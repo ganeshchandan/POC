5d3d99e88f24695dd418a3fc49116ae2812c20bb
ganeshchandan <chandan.ganesh92@gmail.com>
2024-05-05 23:35:22 +0530

    code changes

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -61,6 +61,7 @@ function registerValidSW(swUrl, config) {
     .then((registration) => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
+        console.log("here installingWorker", installingWorker);
         if (installingWorker == null) {
           return;
         }
